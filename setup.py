f68eb720d5b565f1aeae15e5c7822a1bf6876853
Ruben Broekx <broekxruben@gmail.com>
2021-03-08 11:53:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ INSTALL_REQUIRES = [
 # noinspection SpellCheckingInspection
 setup(
         name="semi_supervised_clustering",
-        version="0.4.3",
+        version="0.4.4",
         description="Cluster context-less language data in a semi-supervised manner.",
         long_description=README,
         long_description_content_type="text/markdown",
